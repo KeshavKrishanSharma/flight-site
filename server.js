8fef8d04e2125221731ce168b2ca6f43a5fa4ea2
Keshav Krishan Sharma <keshavkrishansharmaofficial@gmail.com>
2022-10-19 13:15:02 +0530

    done

--- server.js
+++ server.js
@@ -7,7 +7,19 @@ app.use(express.json())
 
 const usersRoute = require('./routes/usersRoute')
 const flightsRoute = require('./routes/flightsRoute')
+const bookingsRoute = require("./routes/bookingsRoute");
 
 app.use('/api/users', usersRoute)
 app.use('/api/flights', flightsRoute)
+app.use("/api/bookings", bookingsRoute);
+
+const path = require("path");
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    });
+}
 app.listen(port, () =>console.log('node server started') );
